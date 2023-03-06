4b6f60cd46c39d7b145057013fac42ac51adfad3
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-06 14:01:58 +0100

    update mobile_body.part

--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -12,13 +12,33 @@ class MyMobileBody extends StatelessWidget {
       ),
       body: Column(
         children: [
+          // youtube video (size sample)
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 250,
-              color: Colors.deepPurple[400],
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                height: 250,
+                color: Colors.deepPurple[400],
+              ),
             ),
-          )
+          ),
+
+          // Comment section / recommended video list (size sample)
+          Expanded(
+            child: ListView.builder(
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color: Colors.deepPurple[300],
+                    height: 120,
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
