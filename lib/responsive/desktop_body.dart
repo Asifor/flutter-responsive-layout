65ec157d82e2e09353a211e7cb0bcb5448a42772
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-06 14:04:03 +0100

    update desktop body

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -5,8 +5,42 @@ class MyDesktopBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.pink,
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[300],
+      appBar: AppBar(
+        title: const Text('D E S K T O P'),
+      ),
+      body: Column(
+        children: [
+          // youtube video (size sample)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                height: 250,
+                color: Colors.deepPurple[400],
+              ),
+            ),
+          ),
+
+          // Comment section / recommended video list (size sample)
+          Expanded(
+            child: ListView.builder(
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color: Colors.deepPurple[200],
+                    height: 120,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
