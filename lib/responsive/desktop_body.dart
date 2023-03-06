5a908e73b21a87bfcddfaa0631ffcbe4d642b8ac
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-06 14:07:19 +0100

    update desktop_body.dart, wrapped column in row so as to split

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -10,34 +10,38 @@ class MyDesktopBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('D E S K T O P'),
       ),
-      body: Column(
+      body: Row(
         children: [
-          // youtube video (size sample)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Container(
-                height: 250,
-                color: Colors.deepPurple[400],
-              ),
-            ),
-          ),
-
-          // Comment section / recommended video list (size sample)
-          Expanded(
-            child: ListView.builder(
-              itemCount: 8,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
+          Column(
+            children: [
+              // youtube video (size sample)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
                   child: Container(
-                    color: Colors.deepPurple[200],
-                    height: 120,
+                    height: 250,
+                    color: Colors.deepPurple[400],
                   ),
-                );
-              },
-            ),
+                ),
+              ),
+
+              // Comment section / recommended video list (size sample)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 8,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        color: Colors.deepPurple[200],
+                        height: 120,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ],
       ),
