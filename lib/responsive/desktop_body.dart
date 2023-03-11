9592c30576fded9b4896055fb0400aa1ad7b0b5d
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-11 05:55:32 +0100

    update desktop_body.dart

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -10,40 +10,55 @@ class MyDesktopBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('D E S K T O P'),
       ),
-      body: Row(
-        children: [
-          Column(
-            children: [
-              // youtube video (size sample)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: Container(
-                    height: 250,
-                    color: Colors.deepPurple[400],
-                  ),
-                ),
-              ),
-
-              // Comment section / recommended video list (size sample)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 8,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            // First Column
+            Expanded(
+              child: Column(
+                children: [
+                  // youtube video (size sample)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AspectRatio(
+                      aspectRatio: 16 / 9,
                       child: Container(
-                        color: Colors.deepPurple[200],
-                        height: 120,
+                        height: 250,
+                        color: Colors.deepPurple[400],
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  ),
+
+                  // Comment section / recommended video list (size sample)
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 8,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.deepPurple[200],
+                            height: 120,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+
+            // Second Column
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 200,
+                color: Colors.deepPurple[300],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
