bd270a740e1a0d6fab5850f7833983d8ee9d75bc
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-11 05:59:32 +0100

    update mobile_body

--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -10,36 +10,39 @@ class MyMobileBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('M O B I L E'),
       ),
-      body: Column(
-        children: [
-          // youtube video (size sample)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Container(
-                height: 250,
-                color: Colors.deepPurple[400],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // youtube video (size sample)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  height: 250,
+                  color: Colors.deepPurple[400],
+                ),
               ),
             ),
-          ),
 
-          // Comment section / recommended video list (size sample)
-          Expanded(
-            child: ListView.builder(
-              itemCount: 8,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Colors.deepPurple[200],
-                    height: 120,
-                  ),
-                );
-              },
+            // Comment section / recommended video list (size sample)
+            Expanded(
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.deepPurple[200],
+                      height: 120,
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
